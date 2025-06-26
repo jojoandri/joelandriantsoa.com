49a2c30eedc1e042ffc6a38e65ded6c267f0f406
JDev <joel.andriantsoa@esti.mg>
2025-06-26 13:52:58 +0300

    Révision

--- src/data.js
+++ src/data.js
@@ -4,7 +4,7 @@ export const projects = [
     description: "Promoting local products and malagasy technology, Mora Market is an ecommerce and marketplace platform that connects consumers with local producers. It secures payment options, and a forwarding service for efficient delivery.",
     src: "moramarket.png",
     link: "https://www.moramarket.mg",
-    color: "#119be3"
+    color: "#ffffff"
   },
   {
     title: "FIARO - Venture Capital",
@@ -17,18 +17,18 @@ export const projects = [
     title: "INVIA - Social media for Investors & Entrepreneurs",
     description: "INVIA is a social media platform designed for investors and entrepreneurs to connect, share ideas, and collaborate on projects. It features a user-friendly interface, secure messaging, and tools for networking and investment opportunities.",
     video: "INVIA.mp4",
-    color: "#84b2e0"
+    color: "#ffffff"
   },
   {
     title: "Pharmaspecific - Clinical Research Organization",
     description: "Pharmaspecific is a Clinical Research Organization (CRO) that provides comprehensive services for clinical trials and research studies. It offers expertise in regulatory compliance, data management, and patient recruitment to ensure successful trial execution.",
     video: "Pharmaspécific.mp4",
-    color: "#956291"
+    color: "#ffffff"
   },
   {
     title: "Quincaillerie Ambodivoara - Hardware Store",
     description: "Quincaillerie Ambodivoara is a hardware store that offers a wide range of tools, materials, and equipment for construction and home improvement projects. It provides quality products and expert advice to help customers with their DIY and professional needs.",
     video: "Quincaillerie-ambodivoara.mp4",
-    color: "#dfc45b"
+    color: "#ffffff"
   },
 ]
