8378d1bbddbdee7117ea8dbcbd985b705ec87e8b
JDev <joel.andriantsoa@esti.mg>
2026-07-14 16:50:07 +0300

    feat(analytics): add google tag manager

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
 import './globals.css';
 import dynamic from 'next/dynamic';
+import Script from 'next/script';
 import Analytics from '../components/Analytics';
 import SmoothScroll from '../components/SmoothScroll';
 
@@ -40,6 +41,13 @@ export default function RootLayout({ children }) {
 
       <head>
         {/* Preload critical resources pour améliorer les performances */}
+        <Script id="gtm" strategy="beforeInteractive">
+          {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-TGTRRMBB');`}
+        </Script>
         <link
           rel="preload"
           href="/fonts/NeueMontreal_Medium.otf"
@@ -58,6 +66,14 @@ export default function RootLayout({ children }) {
       </head>
 
       <body>
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-TGTRRMBB"
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+          />
+        </noscript>
         <Analytics />
         {process.env.NODE_ENV !== 'production' && <AnalyticsDebug />}
         <Header />
